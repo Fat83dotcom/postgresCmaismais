c5627eee11f012a492178bfb73d0403d91113411
fat83dotcom <fernando.quimic@gmail.com>
2023-02-14 19:16:09 -0300

    Criação de struct para gerenciar as tabelas e continuação do desenvolvimento da classe de controle do banco de dados

--- classes.h
+++ classes.h
@@ -10,6 +10,19 @@ using namespace pqxx;
 using std::cerr;
 using std::string;
 
+typedef struct TabelaCliente{
+    string nome_cliente;
+    string cpf_cliente;
+    string data_nasc;
+    string endereco_rua;
+    string endereco_numero;
+    string endereco_bairro;
+    string endereco_cep;
+    string endereco_estado;
+    string endereco_cidade;
+}TabelaCliente;
+
+
 class ObjetosBd{
 public:
     virtual void insertDados() = 0;
@@ -19,9 +32,11 @@ public:
 class ConecPostgres : public ObjetosBd{
 private:
     string config;
+    string sqlInsert;
     
 public:
     ConecPostgres(const string &sConfig);
+    string ConecPostgres::recebeSqlInsert(const string &sql);
     
     ConecPostgres::ConecPostgres(const string &sConfig){
         try{
@@ -32,9 +47,16 @@ public:
         }
     }
 
+    string ConecPostgres::recebeSqlInsert(const string &sql){
+        this->sqlInsert = sql;
+    }
+
     void ConecPostgres::insertDados() override{
         try{
             pqxx::connection conn(this->config);
+            pqxx::work txn(conn);
+            txn.exec0(sqlInsert);
+            txn.commit();
         }
         catch(const std::exception& e){
             std::cerr << e.what() << '\n';
